6bb17a97c9c959c31d746f93bb651eee4138d772
preciouswura09-art <preciouswura09@gmail.com>
2026-07-02 16:11:51 +0100

    none

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+type props = {
+  label: string
+};
 
-const Button = ({ label }) => {
+const Button = ({ label }:props ) => {
   return (
     <button className="bg-green-500 rounded-full w-full py-2.5 font-bold text-black">
       {label}
