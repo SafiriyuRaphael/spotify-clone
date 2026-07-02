34d23cb8dc3642d7d925d093d73348664ebbd8ac
preciouswura09-art <preciouswura09@gmail.com>
2026-07-02 14:19:15 +0100

    spotify-clone

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Button from "../components/Button";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return   ( 
+    <div className="flex flex-col gap-5 items-center ">
+    <h1 className="text-5xl font-bold text-center"> Sign up to <br /> start listening </h1>
+    <label htmlFor="mail" className="mr-56 translate-y-3">Email adresss  </label>
+    <input type="email" id="mail" placeholder="name@domain.com" className="border py-3 w-80 rounded-bl-xs px-2" />
+    <Button label="Next" />
+  </div>
+  );
 };
 
 export default SignUp;
